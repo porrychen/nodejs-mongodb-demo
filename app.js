745eceb1e4bcc05900fd30fe680747423187e98f
Porry Chen <porrychen@localhost.(none)>
2011-12-29 15:42:41 +0800

    修改app.'s，读取表user，修改某条记录的字段值，添加一条记录

--- app.js
+++ app.js
@@ -14,7 +14,50 @@ process.argv.forEach(function (val, index, array) {
     console.log(index + ': ' + val);
     if (val.indexOf('mongodbsrc=') != -1) {
         val = val.replace('mongodbsrc=', '');
-        mongoose.connect(val);
+        mongoose.connect(String(val));
+
+        console.log(String(val) + ' connect success !');
+    }
+});
+
+var TestSchema = new Schema({
+    name        : {type : String}
+    ,age       : {type : String}
+});
+
+var coll_name = 'user';//表名
+
+var USER  = mongoose.model(coll_name, TestSchema);
+
+// 修改
+USER.find({ name: 'genedna'},function(err, users) {
+    if (!err) {
+        users.forEach(function(user){
+            user.age = '2008';
+            user.save();
+        });
+    }
+});
+
+// 查询
+USER.find({name:/^cbl/},function(err, users) {
+    if (!err) {
+        users.forEach(function(user){
+            console.log(user);
+        });
+
+        var user  = new USER();
+        user.name = 'cbl' + users.length;
+        user.age = '2000';
+        //指定插入
+        user.save(function(err) {
+            if (err) {
+                console.log(err);
+                console.log('save failed');
+            } else {
+                console.log('save success');
+            }
+        });
     }
 });
 
@@ -46,5 +89,5 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000);
+app.listen(4000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
