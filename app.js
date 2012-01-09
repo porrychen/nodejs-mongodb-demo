bdfd34c492f235bed012da340a521103006a1171
Porry Chen <porrychen@localhost.(none)>
2012-01-09 14:05:32 +0800

    注释掉app.use(express.bodyParser()); 不然使用formidable什么有种死在那儿的感觉。

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.bodyParser());
+//  app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
   app.use(express.session({ secret: 'your secret here' }));
@@ -57,7 +57,7 @@ var startRouter = function(path, pages) {
             });
         });
     } else {
-        app.post(path.route, function(req,res){
+        app.post(path.route, function(req,res, next){
             console.log("app post "+path.route + util.inspect(req));
             pages.forEach(function(page) {
                 if (page.name == path.page) {
@@ -68,43 +68,49 @@ var startRouter = function(path, pages) {
 
                         form.uploadDir = './data';
 
-                        form
-                            .on('field', function(field, value) {
-                            console.log(field, value);
-                            fields.push([field, value]);
-                        })
-                            .on('file', function(field, file) {
-                                console.log(field, file);
-                                files.push([field, file]);
-                            })
-                            .on('end', function() {
-                                console.log('-> upload done');
-//                                res.writeHead(200, {'content-type': 'text/plain'});
-//                                res.write('received fields:\n\n '+util.inspect(fields));
-//                                res.write('\n\n');
-//                                res.end('received files:\n\n '+util.inspect(files));
-                            });
-                        form.parse(req);
+//                        form
+//                            .on('field', function(field, value) {
+//                            console.log(field, value);
+//                            fields.push([field, value]);
+//                        })
+//                            .on('file', function(field, file) {
+//                                console.log(field, file);
+//                                files.push([field, file]);
+//                            })
+//                            .on('end', function() {
+//                                console.log('-> upload done');
+////                                res.writeHead(200, {'content-type': 'text/plain'});
+////                                res.write('received fields:\n\n '+util.inspect(fields));
+////                                res.write('\n\n');
+////                                res.end('received files:\n\n '+util.inspect(files));
+//                            });
+//                        form.parse(req);
 
-                        res.render(path.template, page);
+//                        res.render(path.template, page);
 
-//                        form.parse(req, function(error, fields, files) {
-//                            var path     = files['file']['path'],
-//                                filename = files['file']['filename'],
-//                                mime     = files['file']['mime'];
-//                            console.log('upload file : ' + path);
-//                            fs.renameSync(files.upload.path, 'test.gif');
-//
-////                            page.imgsrc = filename;
-//
-//                            res.render(path.template, page);
-//                        });
+                        form.parse(req, function(error, fields, files) {
+                            var filepath     = files['upload']['path'],
+                                filename = files['upload']['filename'],
+                                mime     = files['upload']['mime'];
+                            console.log('upload file : ' + filepath);
+                            fs.renameSync(filepath, './data/test.gif');
+
+                            page.imgsrc = './data/test.gif';
+
+                            res.headerSent;
+                            console.log(page.imgsrc);
+                            res.render(path.template, page);
+                        });
+
+
                     } else {
                         console.log('not upload file')
                         res.render(path.template, page);
                     }
                 }
+
             });
+
         });
     }
 
@@ -125,7 +131,66 @@ mongoose.pages(function(error, ps){
 
 //app.get('/', routes.index);
 
-//app.post('/upload', )
+//app.post('/upload1', function(req, res){
+//    console.log(req.body.user);
+//    res.redirect('back');
+//});
+//
+//app.get('/', function(req, res){
+//    res.send('<form method="post" enctype="multipart/form-data">'
+//        + '<p>Image: <input type="file" name="image" /></p>'
+//        + '<p><input type="submit" value="Upload" /></p>'
+//        + '</form>');
+//});
+
+
+
+//app.post('/', function(req, res, next){
+
+    // connect-form adds the req.form object
+    // we can (optionally) define onComplete, passing
+    // the exception (if any) fields parsed, and files parsed
+
+
+//    req.form.complete(function(err, fields, files){
+//        if (err) {
+//            next(err);
+//        } else {
+//            console.log('\nuploaded %s to %s'
+//                ,  files.image.filename
+//                , files.image.path);
+//            res.redirect('back');
+//        }
+//    });
+//
+//    // We can add listeners for several form
+//    // events such as "progress"
+//    req.form.on('progress', function(bytesReceived, bytesExpected){
+//        var percent = (bytesReceived / bytesExpected * 100) | 0;
+//        process.stdout.write('Uploading: %' + percent + '\r');
+//    });
+
+//    var form = new formidable.IncomingForm();
+//    form.uploadDir = './data';
+
+//    form
+//        .on('field', function(field, value) {
+//        console.log(field, value);
+////        fields.push([field, value]);
+//    })
+//        .on('file', function(field, file) {
+//            console.log(field, file);
+////            files.push([field, file]);
+//        })
+//        .on('end', function() {
+//            console.log('-> upload done');
+////                                res.writeHead(200, {'content-type': 'text/plain'});
+////                                res.write('received fields:\n\n '+util.inspect(fields));
+////                                res.write('\n\n');
+////                                res.end('received files:\n\n '+util.inspect(files));
+//        });
+//    form.parse(req);
+//});
 
 app.listen(4000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
